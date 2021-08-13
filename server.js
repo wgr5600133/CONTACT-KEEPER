403e9f35ffc65632994e3b955158c79e49253676
wgr5600133 <GUW18@pitt.edu>
2021-08-13 18:00:03 -0400

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to ContactKeeper API..." })
+);
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contact", require("./routes/contacts"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
